06d983b84f449b164bfa4301d24e06c84356feb7
OGSegu <seguisreal@gmail.com>
2020-11-05 23:20:57 +0300

    Bloom filter has been done

--- src/main/java/BloomFilter.java
+++ src/main/java/BloomFilter.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class BloomFilter {
 
-    public static final double error_probability = 0.01;
-
     private final byte[] byteArray;
-    private final int hashFuncAmount;
+    private final List<HashFunction> hashesList = new ArrayList<>();
 
-    BloomFilter(int elementsAmount) {
+    private final int hashFuncAmount;
+    public final double error_probability;
+
+    BloomFilter(int elementsAmount, double errorProbability) {
         if (elementsAmount < 1)
             throw new IllegalArgumentException();
-        byteArray = new byte[getOptimalSize(elementsAmount)];
-        hashFuncAmount = getHashFuncAmount(elementsAmount);
+        this.error_probability = errorProbability;
+        this.byteArray = new byte[getOptimalSize(elementsAmount)];
+        this.hashFuncAmount = getHashFuncAmount(elementsAmount);
+        generateHashes();
     }
 
     public int getOptimalSize(int elementsAmount) {
@@ -19,4 +26,41 @@ public class BloomFilter {
     public int getHashFuncAmount(int elementsAmount) {
         return (int) (byteArray.length / elementsAmount * Math.log(2));
     }
+
+    private void generateHashes() {
+        Random random = new Random();
+        for (int i = 0; i < hashFuncAmount; i++)
+            hashesList.add(new HashFunction(random.nextInt(50) + 1));
+        }
+
+    public void add(String element) {
+        for (HashFunction hashFunction : hashesList) {
+            int index = hashFunction.getHash(element);
+            byteArray[index] = 1;
+        }
+    }
+
+    public boolean contains(String element) {
+        boolean result = true;
+        for (HashFunction hashFunction : hashesList) {
+            int index = hashFunction.getHash(element);
+            result &= byteArray[index] == 1;
+        }
+        return result;
+    }
+
+    public class HashFunction {
+
+        private final int seed;
+
+        HashFunction(int seed) {
+            this.seed = seed;
+        }
+
+        public int getHash(String element) {
+            int hash = (Math.abs(element.hashCode()) / seed) % byteArray.length;
+            return hash;
+        }
+    }
+
 }
--- src/test/java/BloomFilterTest.java
+++ src/test/java/BloomFilterTest.java
@@ -7,23 +7,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BloomFilterTest {
 
-    private static final int elementsAmount = 1000;
+    private static final int elementsAmount = 5;
+    public static final double error_probability = 0.01;
 
-    BloomFilter bloomFilter = new BloomFilter(elementsAmount);
+    BloomFilter bloomFilter = new BloomFilter(elementsAmount, error_probability);
 
     private final Logger logger = Logger.getLogger(BloomFilterTest.class.getName());
 
     @Test
     void getOptimalSize() {
         int optimalSize = bloomFilter.getOptimalSize(elementsAmount);
-        logger.log(Level.INFO, "Optimal size per 1k with " + BloomFilter.error_probability + "% for error: " + optimalSize);
+        logger.log(Level.INFO, "Optimal size per 1k with " + error_probability + "% for error: " + optimalSize);
         assertEquals(9585, optimalSize);
     }
 
     @Test
     void getHashFuncAmount() {
         int hashFuncAmount = bloomFilter.getHashFuncAmount(elementsAmount);
-        logger.log(Level.INFO, "Optimal hash func amount with " + BloomFilter.error_probability + "% for error: " + hashFuncAmount);
+        logger.log(Level.INFO, "Optimal hash func amount with " + error_probability + "% for error: " + hashFuncAmount);
         assertEquals(6, hashFuncAmount);
+    }
+
+    @Test
+    void add() {
+        String[] array = {"vadim", "misha", "dasha", "regina", "ilya"};
+        for (String str : array) {
+            bloomFilter.add(str);
+        }
+    }
+
+    @Test
+    void contains() {
+        String[] array = {"vadim", "kolya", "ilyas", "artem", "oleg"};
+        for (String str : array) {
+            logger.log(Level.INFO, bloomFilter.contains(str) ?  str + ": probably yes" : str + ": no");
+        }
     }
 }
